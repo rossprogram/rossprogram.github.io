04b20f7249ef17abf2effc95ae2a5e46f6581099
Joshua Friedman <4homeworkstuff@gmail.com>
2020-04-13 17:04:42 -0700

    yes

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,361 @@
+var store = [{
+        "title": "Barrier problem née archer problem",
+        "excerpt":"Submitted by Matthew Stone (Ross counselor and Yale undergraduate) A collection of line segments contained in a unit square is said to be a barrier if every straight line that crosses the square cuts one of those line segments.  Note: A barrier is not required to be connected. [caption id=\"attachment_498\"...","categories": ["Problems"],
+        "tags": [],
+        "url": "http://localhost:4000/problems/barrier-problem/",
+        "teaser": null
+      },{
+        "title": "1965",
+        "excerpt":"          Group Photo, 1965 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1965/",
+        "teaser": null
+      },{
+        "title": "1966",
+        "excerpt":"          Group Photo, 1966 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1966/",
+        "teaser": null
+      },{
+        "title": "1967",
+        "excerpt":"          Group Photo, 1967 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1967/",
+        "teaser": null
+      },{
+        "title": "1968",
+        "excerpt":"          Group Photo, 1968 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1968/",
+        "teaser": null
+      },{
+        "title": "1969",
+        "excerpt":"          Group Photo, 1969 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1969/",
+        "teaser": null
+      },{
+        "title": "1970",
+        "excerpt":"          Group Photo, 1970 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1970/",
+        "teaser": null
+      },{
+        "title": "1971",
+        "excerpt":"          Group Photo, 1971 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1971/",
+        "teaser": null
+      },{
+        "title": "1972",
+        "excerpt":"          Group Photo, 1972 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1972/",
+        "teaser": null
+      },{
+        "title": "1973",
+        "excerpt":"          Group Photo, 1973 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1973/",
+        "teaser": null
+      },{
+        "title": "1974",
+        "excerpt":"          Group Photo, 1974 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1974/",
+        "teaser": null
+      },{
+        "title": "1975",
+        "excerpt":"          Group Photo, 1975 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1975/",
+        "teaser": null
+      },{
+        "title": "1976",
+        "excerpt":"          Group Photo, 1976 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1976/",
+        "teaser": null
+      },{
+        "title": "1977",
+        "excerpt":"          Group Photo, 1977 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1977/",
+        "teaser": null
+      },{
+        "title": "1978",
+        "excerpt":"          Group Photo, 1978 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1978/",
+        "teaser": null
+      },{
+        "title": "1979",
+        "excerpt":"          Group Photo, 1979 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1979/",
+        "teaser": null
+      },{
+        "title": "1980",
+        "excerpt":"          Group Photo, 1980 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1980/",
+        "teaser": null
+      },{
+        "title": "1981",
+        "excerpt":"          Group Photo, 1981 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1981/",
+        "teaser": null
+      },{
+        "title": "1982",
+        "excerpt":"          Group Photo, 1982 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1982/",
+        "teaser": null
+      },{
+        "title": "1983",
+        "excerpt":"          Group Photo, 1983 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1983/",
+        "teaser": null
+      },{
+        "title": "1984",
+        "excerpt":"          Group Photo, 1984 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1984/",
+        "teaser": null
+      },{
+        "title": "1985",
+        "excerpt":"          Group Photo, 1985 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1985/",
+        "teaser": null
+      },{
+        "title": "1986",
+        "excerpt":"          Group Photo, 1986 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1986/",
+        "teaser": null
+      },{
+        "title": "1987",
+        "excerpt":"          Group Photo, 1987 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1987/",
+        "teaser": null
+      },{
+        "title": "1988",
+        "excerpt":"          Group Photo, 1988 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1988/",
+        "teaser": null
+      },{
+        "title": "1989",
+        "excerpt":"          Group Photo, 1989 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1989/",
+        "teaser": null
+      },{
+        "title": "1990",
+        "excerpt":"          Group Photo, 1990 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1990/",
+        "teaser": null
+      },{
+        "title": "1991",
+        "excerpt":"          Group Photo, 1991 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1991/",
+        "teaser": null
+      },{
+        "title": "1992",
+        "excerpt":"          Group Photo, 1992 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1992/",
+        "teaser": null
+      },{
+        "title": "1993",
+        "excerpt":"          Group Photo, 1993 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1993/",
+        "teaser": null
+      },{
+        "title": "1994",
+        "excerpt":"          Group Photo, 1994 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1994/",
+        "teaser": null
+      },{
+        "title": "1995",
+        "excerpt":"          Group Photo, 1995 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1995/",
+        "teaser": null
+      },{
+        "title": "1996",
+        "excerpt":"          Group Photo, 1996 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1996/",
+        "teaser": null
+      },{
+        "title": "1997",
+        "excerpt":"          Group Photo, 1997 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1997/",
+        "teaser": null
+      },{
+        "title": "1998",
+        "excerpt":"          Group Photo, 1998 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1998/",
+        "teaser": null
+      },{
+        "title": "1999",
+        "excerpt":"          Group Photo, 1999 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/1999/",
+        "teaser": null
+      },{
+        "title": "2000",
+        "excerpt":"          Group Photo, 2000 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2000/",
+        "teaser": null
+      },{
+        "title": "2001",
+        "excerpt":"          Group Photo, 2001 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2001/",
+        "teaser": null
+      },{
+        "title": "2002",
+        "excerpt":"          Group Photo, 2002 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2002/",
+        "teaser": null
+      },{
+        "title": "2003",
+        "excerpt":"          Group Photo, 2003 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2003/",
+        "teaser": null
+      },{
+        "title": "2004",
+        "excerpt":"          Group Photo, 2004 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2004/",
+        "teaser": null
+      },{
+        "title": "2005",
+        "excerpt":"          Group Photo, 2005 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2005/",
+        "teaser": null
+      },{
+        "title": "2006",
+        "excerpt":"          Group Photo, 2006 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2006/",
+        "teaser": null
+      },{
+        "title": "2007",
+        "excerpt":"          Group Photo, 2007 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2007/",
+        "teaser": null
+      },{
+        "title": "2008",
+        "excerpt":"          Group Photo, 2008 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2008/",
+        "teaser": null
+      },{
+        "title": "2009",
+        "excerpt":"          Group Photo, 2009 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2009/",
+        "teaser": null
+      },{
+        "title": "2010",
+        "excerpt":"          Group Photo, 2010 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2010/",
+        "teaser": null
+      },{
+        "title": "2011",
+        "excerpt":"          Group Photo, 2011 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2011/",
+        "teaser": null
+      },{
+        "title": "2012",
+        "excerpt":"          Group Photo, 2012 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2012/",
+        "teaser": null
+      },{
+        "title": "2013",
+        "excerpt":"          Group Photo, 2013 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2013/",
+        "teaser": null
+      },{
+        "title": "2014",
+        "excerpt":"          Group Photo, 2014 Ross Program with key   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2014/",
+        "teaser": null
+      },{
+        "title": "2015",
+        "excerpt":"          Group Photo, 2015 Ross Program with key    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2015/",
+        "teaser": null
+      },{
+        "title": "2016 Asia",
+        "excerpt":"          Group Photo, 2016 Ross/Asia Program   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2016-asia/",
+        "teaser": null
+      },{
+        "title": "2016",
+        "excerpt":"          Group Photo, 2016 Ross Program with key    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2016/",
+        "teaser": null
+      },{
+        "title": "2017 Asia",
+        "excerpt":"          Group Photo, 2017 Ross/Asia Program   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2017-asia/",
+        "teaser": null
+      },{
+        "title": "2017 Usa",
+        "excerpt":"          Group Photo, 2017 Ross/USA Program    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2017-usa/",
+        "teaser": null
+      },{
+        "title": "2018 Asia",
+        "excerpt":"          Group Photo, 2018 Ross/Asia Program    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2018-asia/",
+        "teaser": null
+      },{
+        "title": "2018 Usa",
+        "excerpt":"          Group Photo, 2018 Ross/USA Program    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2018-usa/",
+        "teaser": null
+      },{
+        "title": "2019 Asia",
+        "excerpt":"          Group Photo, 2019 Ross/Asia Program   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2019-asia/",
+        "teaser": null
+      },{
+        "title": "2019 Usa",
+        "excerpt":"          Group Photo, 2019 Ross/USA Program    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/years/2019-usa/",
+        "teaser": null
+      }]
